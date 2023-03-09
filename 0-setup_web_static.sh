5b2cb149dcdc4381441fdedf5872363a46c58925
barasa001 <s.wanyonyi@alustudent.com>
2023-03-09 07:31:09 -0800

    the commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,1 +1,27 @@
 #!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+
+#update list
+sudo apt-get update
+
+# Install Nginx
+sudo apt-get install -y nginx
+
+# Create the folder /data/web_static containing releases, shared and releases/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file /data/web_static/releases/test/index.html
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
+
+#Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+#Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restart
+sudo service nginx restart
