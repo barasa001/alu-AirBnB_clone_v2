9f0e948ab5892ec7d9513dc586cb51063a266496
barasa001 <s.wanyonyi@alustudent.com>
2023-03-09 08:47:21 -0800

    commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""compress a folder"""
+from datetime import datetime
+from fabric.api import *
+import os
+import shlex
+
+env.hosts = ['18.209.7.164', '54.211.25.155']
+env.user = ['ubuntu']
+
+def do_deploy(archive_path):
