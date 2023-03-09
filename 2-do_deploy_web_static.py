4f745a232373fb35d82b7a5e03130bb2dd8c0e11
barasa001 <s.wanyonyi@alustudent.com>
2023-03-09 09:56:53 -0800

    the commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,21 +10,17 @@ env.user = ['ubuntu']
 
 def do_deploy(archive_path):
     """Distribute an archive to the web servers."""
-    if not path.exists(archive_path):
+    if archive_path is None or not os.path.exists(archive_path):
         return False
-    file_name = path.basename(archive_path)
-    folder_name = file_name.replace('.tgz', '')
-    remote_path = '/tmp/{}'.format(file_name)
-    put(archive_path, remote_path)
-    run('mkdir -p /data/web_static/releases/{}'.format(folder_name))
-    run('tar -xzf {} -C /data/web_static/releases/{}'.format
-            (remote_path, folder_name))
-    run('rm {}'.format(remote_path))
-    run('mv /data/web_static/releases/{}/web_static/* 
-            /data/web_static/releases/{}/'.format
-            (folder_name, folder_name))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format
-            (folder_name))
-    return True
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}".format(archive_path))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}".format(
+            archive_path))
+        run("rm /tmp/{}.tgz".format(archive_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".
+                format(archive_path))
+        return True
+    except BaseException:
+        return False
