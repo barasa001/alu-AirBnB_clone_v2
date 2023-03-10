9dac9f57a4c999f8edc1029d4d207351ba224915
barasa254 <101549153+barasa001@users.noreply.github.com>
2023-03-10 08:42:33 +0300

    changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,17 +11,33 @@ env.user = ['ubuntu']
 
 def do_deploy(archive_path):
     """Distribute an archive to the web servers."""
-    if archive_path is None or not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
-    try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(archive_path))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}".format(
-            archive_path))
-        run("rm /tmp/{}.tgz".format(archive_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".
-            format(archive_path))
-        return True
-    except BaseException:
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
