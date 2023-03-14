62614f2b763fb805709b5242998b7fe892d795dd
barasa001 <s.wanyonyi@alustudent.com>
2023-03-13 19:31:54 -0700

    file five

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,5 +40,11 @@ def number(n):
     return "%d is a number" % n
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Function for /number_template/<n> route"""
+    return render_template("5-number.html", n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
