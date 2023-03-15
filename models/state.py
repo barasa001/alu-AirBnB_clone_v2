758ebccd1c0fb4c8f90f7fed95c665e240cb0da5
barasa254 <101549153+barasa001@users.noreply.github.com>
2023-03-15 05:27:59 +0300

    changes

--- models/state.py
+++ models/state.py
@@ -4,6 +4,11 @@ from models.base_model import BaseModel, Base
 # import sqlalchemy modules
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import shlex
+import models
+from models.city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(BaseModel, Base):
@@ -18,12 +23,17 @@ class State(BaseModel, Base):
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """function to return list of cities"""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list        
+     @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)        
