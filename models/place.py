812e3556269818c6285a49cdb312552a39d055f0
barasa001 <s.wanyonyi@alustudent.com>
2023-03-23 04:13:05 +0000

    commit

--- models/place.py
+++ models/place.py
@@ -27,7 +27,6 @@ association_table = Table("place_amenity", Base.metadata,
 class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
     Inherits from SQLAlchemy Base and links to the MySQL table places.
-    Attributes:
     """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
